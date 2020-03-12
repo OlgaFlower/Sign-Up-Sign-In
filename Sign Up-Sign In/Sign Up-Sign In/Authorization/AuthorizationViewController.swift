31b84a6cf3ebac3979d254db44fb94dc5705e138
Olga <kindzyadzyu@gmail.com>
2020-03-12 16:00:25 +0200

    start screen

---
+++ Sign Up-Sign In/Sign Up-Sign In/Authorization/AuthorizationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AuthorizationViewController.swift
+//  Sign Up-Sign In
+//
+//  Created by Admin on 12.03.2020.
+//  Copyright © 2020 Flower. All rights reserved.
+//
+
+import UIKit
+
+class AuthorizationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
